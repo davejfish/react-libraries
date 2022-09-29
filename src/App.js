b97c1b513535121cfa70aaeb3db46a5cc9d951e6
Dave Fisher <dave.j.fish@gmail.com>
2022-09-29 14:10:13 -0700

    working table with data

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Table from './components/Table/Table';
 
 function App() {
   return (
-    <div className="App">
+    <div className="content">
       <nav className='navbar'>
         <Link to="/table" className='navbar-item'>Table</Link>
         <Link to="/charts" className='navbar-item'>Charts</Link>
       </nav>
       <Switch>
         <Route path="/table">
-          <h1>Table Library</h1>
+          <h1 className='title'>Table Library</h1>
           <Table />
         </Route>
         <Route path="/charts">
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -7,14 +7,66 @@ import { UserContext } from '../../Context/userContext';
 export default function Table() {
   const { users } = useContext(UserContext);
 
+  // used for alternate way to make a table
+  // const columns = [
+  //   {
+  //     title: 'first',
+  //     key: 'name',
+  //     dataKey: 'first_name',
+  //     width: 100,
+  //   },
+  //   {
+  //     title: 'last',
+  //     key: 'last',
+  //     dataKey: 'last_name',
+  //     width: 100,
+  //   },
+  //   {
+  //     title: 'fav color',
+  //     key: 'color',
+  //     dataKey: 'favorite_color',
+  //     width: 100,
+  //     cellRenderer: ({ cellData }) => {
+  //       return <div className='circle' style={{ backgroundColor : cellData.favorite_color }} />;
+  //     }
+  //   },
+  //   {
+  //     title: 'avatar',
+  //     key: 'image',
+  //     dataKey: 'image',
+  //     width: 100,
+  //     cellRenderer: ({ cellData }) => {
+  //       return <img src={cellData.image} />;
+  //     }
+  //   }
+  // ];
+
   return (
-    <BaseTable data={users} width={500} height={500}>
+    <BaseTable data={users} width={700} height={500}>
       <Column title='first name' key='first' dataKey='first_name' width={100} />
       <Column title='last name' key='last' dataKey='last_name' width={100} />
-      <Column title='fav color' key='color' dataKey='favorite_color' width={100} />
-      <Column title='avatar' key='image' dataKey='image' width={100} cellRenderer={({ rowData }) => {
-        return <img src={rowData.image} />;
-      }} />
+      <Column title='fav color' key='color' width={100} align='center' cellRenderer={
+        ({ rowData }) => (
+          <div className='box' style={{ backgroundColor:rowData.favorite_color }} />
+        )} />
+      <Column title='avatar' key='image' dataKey='image' width={100} cellRenderer={
+        ({ rowData }) => (
+          <div className='box'>
+            <img className='image is-48x48' src={rowData.image} />
+          </div> 
+        )} />
+      <Column title='email' key='email' width={300} align='center' cellRenderer={
+        ({ rowData }) => (
+          <span className='box'>{rowData.email}</span>
+        )
+      } />
     </BaseTable>
+    
+    // docs says this works but it crashes my webpage
+    // <Table data={users}>
+    //   {columns.map(column => (
+    //     <Column key={column.key} {...column} />
+    //   ))}
+    // </Table>
   );
 }
